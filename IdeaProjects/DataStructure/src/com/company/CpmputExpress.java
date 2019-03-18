be7b6b61853d2ad8340d1999b3e6ea3bc32c06e5
daodao <188037271@qq.com>
2019-03-18 17:02:51 +0800

    1

---
+++ IdeaProjects/DataStructure/src/com/company/CpmputExpress.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.lang.Character;
+public class CpmputExpress {
+    private String expressString;
+
+    public CpmputExpress(String expressString) {
+        this.expressString = expressString;
+    }
+//    public Double Compute(){
+//        char[] StringChar = expressString.toCharArray();
+//        MystackByLinkedList<Double> NumberStack = new MystackByLinkedList<>();
+//        MystackByLinkedList<String> Symbol = new MystackByLinkedList<>();
+//        for (int i = 0; i < StringChar.length; i++){
+//            if (Character.isDigit(StringChar[i])){
+//                NumberStack.push(Double)
+//            }
+//        }
+//    }
+
+}
